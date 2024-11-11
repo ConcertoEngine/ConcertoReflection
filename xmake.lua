86eac9a0f561fcad6af2b19a99e12b2afc7e7c6b
Arthur Vasseur <arthur.vasseur@epitech.eu>
2024-11-11 20:16:05 +0100

    Enhance xmake.lua configuration for XML processing; add autoupdate rule and improve file handling for generated headers and sources

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,7 @@ add_rules("mode.debug", "mode.release")
 add_repositories("Concerto-xrepo https://github.com/ConcertoEngine/xmake-repo.git main")
 add_requires("concerto-core", {configs = {debug = is_mode("debug"), with_symbols = true}})
 add_requires("catch2")
+add_rules("plugin.vsxmake.autoupdate")
 
 if is_plat("windows") then
     set_runtimes(is_mode("debug") and "MDd" or "MD")
@@ -9,12 +10,18 @@ end
 
 rule("xml")
     set_extensions(".xml")
-    on_build_file(function (target, sourcefile)
-        os.execv("cabal", {"run", "Reflection", "--", sourcefile, target:targetdir() .. "/Generated/"})
-        target:add("headerfiles", target:targetdir() .. "/(Generated/**.hpp)")
-        target:add("includedirs", target:targetdir() .. "/Generated/", {public = true})
-        target:add("files", target:targetdir() .. "/Generated/**.cpp")
-	end)
+    on_config(function (target)
+        for _, filepath in ipairs(os.files("Tests/**.xml")) do
+            local generatedFile = path.join(target:targetdir(), "Generated", path.basename(filepath))
+            target:add("headerfiles", target:targetdir() .. "/(Generated/" .. path.basename(filepath) ..".hpp)")
+            target:add("includedirs", path.join(target:targetdir(), "Generated"), {public = true})
+            target:add("files", generatedFile .. ".cpp", {always_added = true})
+            target:add("files", filepath)
+        end
+    end)
+    before_build_file(function (target, sourcefile)
+        os.execv("cabal", {"run", "Reflection", "--", sourcefile, path.join(target:targetdir(), "Generated")})
+    end)
 
 target("concerto-reflection")
     set_kind("shared")
@@ -28,6 +35,10 @@ target("concerto-reflection")
 target("concerto-reflection-tests")
     set_kind("binary")
     set_languages("cxx20")
-    add_files("Tests/*.cpp", "sample.xml")
+    add_files("Tests/*.cpp")
     add_packages("catch2")
     add_deps("concerto-reflection")
+    add_rules("xml")
+    if is_plat("windows") then
+        add_cxflags("/Zc:preprocessor")
+    end
