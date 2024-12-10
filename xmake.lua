55f8ecf5f1f40a47c1e93f716d2428eb99188597
Arthur Vasseur <arthur.vasseur@epitech.eu>
2024-12-10 16:19:14 +0100

    Refactor SamplePackage.xml member names and update xmake.lua for package generator integration

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,9 @@
 add_rules("mode.debug", "mode.release")
 add_repositories("Concerto-xrepo https://github.com/ConcertoEngine/xmake-repo.git main")
-add_requires("concerto-core", "eventpp", {configs = {debug = is_mode("debug"), with_symbols = true}})
+add_repositories("local-repo build")
+
+add_requires("concerto-core", "pugixml", "eventpp", "concerto-pkg-generator", {configs = {debug = is_mode("debug"), with_symbols = true}})
+
 add_requires("catch2")
 add_rules("plugin.vsxmake.autoupdate")
 
@@ -8,10 +11,32 @@ if is_plat("windows") then
     set_runtimes(is_mode("debug") and "MDd" or "MD")
 end
 
+target("concerto-pkg-generator")
+    set_kind("binary")
+    set_languages("cxx20")
+    add_files("Src/PackageGenerator/*.cpp")
+    add_headerfiles("Include/(Concerto/PackageGenerator/**.hpp)", "Include/(Concerto/PackageGenerator/**.inl)")
+    add_includedirs("Include/", { public = true })
+    add_packages("concerto-core", "pugixml")
+
+rule("find_cct_pkg_generator")
+    on_config(function (target)
+        import("core.project.project")
+        import("lib.detect.find_tool")
+
+        local cctPkgGen = project.required_package("concerto-pkg-generator")
+        local dir
+        if cctPkgGen then
+            dir = path.join(cctPkgGen:installdir(), "bin")
+        end
+        local program = find_tool("concerto-pkg-generator", {version = false, paths = dir})
+        target:data_set("concerto-pkg-generator", program)
+    end)
+
 rule("xml")
     set_extensions(".xml")
+    add_deps("find_cct_pkg_generator")
     on_config(function (target)
-        --os.vrunv("cabal", {"install", "--installdir=" .. target:targetdir(), "--overwrite-policy=always"})
         for _, filepath in ipairs(target:sourcebatches()["xml"].sourcefiles) do
             local generatedFile = path.join(target:autogendir(), "Reflection", path.basename(filepath))
             target:add("headerfiles", target:autogendir() .. "/(Reflection/" .. path.basename(filepath) ..".hpp)")
@@ -23,13 +48,16 @@ rule("xml")
     end)
 
     before_buildcmd_file(function (target, batchcmds, xmlFile, opt)
+        local cctPkgGen = target:data("concerto-pkg-generator")
+        print("cctPkgGen: ", cctPkgGen)
+        assert(cctPkgGen, "concerto-pkg-generator not found!")
         local outputFolder = path.join(target:autogendir(), "Reflection")
         local outputCppFile = path.join(outputFolder, path.basename(xmlFile) .. ".cpp")
         local outputHppFile = path.join(outputFolder, path.basename(xmlFile) .. ".hpp")
         local exePrefix = target:is_plat("mingw", "windows") and ".exe" or ""
 
         batchcmds:show_progress(opt.progress, "${color.build.object}compiling.reflection %s", xmlFile)
-        batchcmds:vrunv(path.join(target:targetdir(), "Reflection" .. exePrefix), {xmlFile, path.join(target:autogendir(), "Reflection")})
+        batchcmds:vrunv(cctPkgGen.program, {xmlFile, path.join(target:autogendir(), "Reflection")})
 
         batchcmds:add_depfiles(xmlFile)
 		--batchcmds:add_depvalues() todo add version from cabal
