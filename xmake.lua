0ac59b7074c10aa035c64d31e2b6f63da2c73911
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-01-12 15:30:00 +0100

    Refactor find_concerto_pkg_generator.lua to remove debug print statement and update xmake.lua to streamline required packages

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
 add_rules("mode.debug", "mode.release", "mode.coverage")
+add_rules("plugin.vsxmake.autoupdate")
+
 add_repositories("concerto-xrepo https://github.com/ConcertoEngine/xmake-repo.git main")
 
-add_requires("concerto-core", "pugixml", "eventpp", "catch2", {configs = {debug = is_mode("debug"), with_symbols = true}})
-
-add_rules("plugin.vsxmake.autoupdate")
+add_requires("concerto-core", "pugixml", {configs = {debug = is_mode("debug"), with_symbols = true}})
 
 option("tests", { default = false, description = "Enable unit tests"})
 
@@ -41,6 +41,8 @@ target("concerto-reflection")
 
 
 if has_config("tests") then
+    add_requires("catch2")
+
     target("concerto-reflection-tests")
         set_kind("binary")
         set_languages("cxx20")
