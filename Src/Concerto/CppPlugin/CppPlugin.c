0e36e934f840ddf883d10bc2bda20832260e7150
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2026-02-20 15:56:43 +0100

    feat: update macOS runner version to 15 and install clang 20

--- Src/Concerto/CppPlugin/CppPlugin.c
+++ Src/Concerto/CppPlugin/CppPlugin.c
@@ -323,6 +323,13 @@ static void BeforeClassGeneration(const CrpClass* cls, CrpGenerationContext* ctx
 			if (baseClass && baseClass[0] != '\0')
 			{
 				crpGenerationContextWrite(ctx, "const Class* baseClass = GetClassByName(\"%s\"sv);", baseClass);
+				crpGenerationContextWrite(ctx, "if (baseClass == nullptr)");
+				crpGenerationContextEnterScope(ctx);
+				{
+					if (nsPath && nsPath[0] != '\0')
+						crpGenerationContextWrite(ctx, "baseClass = GetClassByName(\"%s::%s\"sv);", nsPath, baseClass);
+				}
+				crpGenerationContextLeaveScope(ctx, NULL);
 				crpGenerationContextWrite(ctx, "CCT_ASSERT(baseClass != nullptr, \"Could not find class '%s'\");", baseClass);
 				crpGenerationContextWrite(ctx, "SetBaseClass(baseClass);");
 			}
